a1b22cce5108546e185d31cfe59273fc16fc398e
bmCodes <moreno.bryan20@yahoo.com>
2020-05-26 23:07:28 -0400

    Initial commit

---
+++ src/main/java/com/managemenu/ManageMenuApplication.java
@@ -0,0 +1,13 @@
+package com.managemenu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageMenuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManageMenuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/managemenu/ManageMenuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.managemenu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManageMenuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
